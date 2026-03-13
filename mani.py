7db2ab422997aad29bb0c50856433f63f9a08dc4
manikandanmani0910-source <manikandanmani0910@gmail.com>
2026-03-13 10:26:11 +0530

    Add files via upload

---
+++ mani.py
@@ -0,0 +1,59 @@
+##Task 1 – Print Formatting
+##Print the following output using sep and end.
+print("Hello world",end =" ")
+print ("welcome python")
+print ("Laptop","Mouse","Keyboard",sep="/")
+##Task 2 – Variables
+name = "Ravi"
+age = 22
+city = "Chennai"
+print(name,age,city,sep="-")
+##ask 3 – Multiple Assignment
+name = "Meena"
+age = 20
+student = True
+print("student information -",name,age,student)
+## Task 4 – Indexing
+## Task  – Indexing
+word = "Python"
+print(word[0])
+print(word[2])
+print(word[5])
+## Task 5 – Arithmetic Operators
+##Calculate and print:
+print(25 + 10)
+print(50 - 20)
+print(8 * 5)
+print(100 / 10)
+print(10 % 30)  
+print(2 ** 4)
+print(20 // 3)
+##Task 6 – BODMAS Expression
+##Find the result:
+print(3 + 2 * 5 ** 2)
+## Task 7 – Assignment Operator
+
+num = 50
+num += 25
+print("final value is;",num)
+num = 100
+num /= 10
+print("print the result:",num)
+## Task 10 – Logical Operators
+print(10 > 5 and 5 == 5)
+print(5 > 10 or 10 == 10)
+print(not(5 > 2))
+##Task 11 – Membership Operator
+numbers = [10,20,30,40,50]
+print(20 in numbers)
+print(60 in numbers)
+print(30 not in numbers)
+##Task 12 – Swap Variables
+a = 10
+b = 20
+a,b=b,a
+print(a,b)
+##Task 13 – Bitwise XOR
+a = 6
+b = 3
+print(a ^ b)
